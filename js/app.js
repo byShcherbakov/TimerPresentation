6fd5b61fa0264a18bf4503c9df42d2ba40e185ed
ShchcerbakoVV <ilya.shcherbakov.05@list.ru>
2026-03-20 10:36:31 +0300

    PC JSbadproblem

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log("dsfdsfs")
+const items = document.querySelectorAll('.countDown-item >h4'); //получать цифру с h4
+
+//Точка отсчета
+let conutdownDay = new Date(2026,2,20,10,24,60).getTime()
+
+
+function getCountDownTime(){
+    //время
+    const now = new Date().getTime(); //получение временив в милл сек
+
+
+    //разница во времени
+    const distance =  conutdownDay - now;
+}
